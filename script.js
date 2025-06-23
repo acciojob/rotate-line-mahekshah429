6c875f938f66d99cf3a960a660b14f0852158600
Mahek shah <149513184+mahekshah429@users.noreply.github.com>
2025-06-23 08:26:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-cy.get('#line')
-  .should('have.css', 'position', 'absolute')
-  .and('have.css', 'width', '200px')
-  .and('have.css', 'height', '2px')
-  .and('have.css', 'background-color', 'rgb(0, 0, 0)');
+describe('Line rotation', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
 
-cy.get('#line').then($el => {
-  const start = $el.css('transform');
-  cy.wait(500).then(() => {
-    const after = $el.css('transform');
-    expect(after).not.to.equal(start);
+  it('should have a line with the correct properties', () => {
+    cy.get('#line')
+      .should('have.css', 'position', 'absolute')
+      .and('have.css', 'width', '200px')
+      .and('have.css', 'height', '2px')
+      .and('have.css', 'background-color', 'rgb(0, 0, 0)');
+  });
+
+  it('should rotate the line', () => {
+    cy.get('#line').then($el => {
+      const initialTransform = $el.css('transform');
+      cy.wait(500).then(() => {
+        const updatedTransform = $el.css('transform');
+        expect(updatedTransform).not.to.equal(initialTransform);
+      });
+    });
   });
 });
