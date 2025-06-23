aeeff905a6eccc73594a80188492fbbe14d81f2a
Mahek shah <149513184+mahekshah429@users.noreply.github.com>
2025-06-23 08:19:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const line = document.getElementById('line');
-let angle = 0;
+const line = document.getElementById("line");
 
-setInterval(() => {
-  angle += 2;
-  line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
-}, 20);
+// Example: Pause rotation on click, resume on second click
+let isRotating = true;
+
+line.addEventListener("click", () => {
+  if (isRotating) {
+    line.style.animationPlayState = "paused";
+  } else {
+    line.style.animationPlayState = "running";
+  }
+  isRotating = !isRotating;
+});
